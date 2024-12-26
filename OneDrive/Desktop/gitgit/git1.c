c0de6b244e0c099ac36e0954b64372502c520547
reddykesava <reddykesavachintala.2001@gmail.com>
2024-12-26 11:15:46 +0530

    commit the code

---
+++ OneDrive/Desktop/gitgit/git1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+//#include<stdlib.h>
+
+int main()
+{
+  /*  int **arr;
+    int row=2;
+    int col=3;
+    int nrow=4;
+    arr=(int **)malloc(row*sizeof(int *));
+    for(int i=0;i<row;i++)
+    {
+        arr[i]=(int *)malloc(col*sizeof(int));
+        for(int j=0;j<col;j++)
+        {
+            arr[i][j]=i*col+j;
+        }
+    }
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    arr=(int **)realloc(arr,nrow*sizeof(int *));
+    for(int i=0;i<nrow;i++)
+    {
+        arr[i]=(int *)malloc(col*sizeof(int));
+        for(int j=0;j<col;j++)
+        {
+            arr[i][j]=i*col+j;
+        }
+    }
+    printf("after:  \n");
+    for(int i=0;i<nrow;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    */
+   printf("hii");
+    return 0;
+}
